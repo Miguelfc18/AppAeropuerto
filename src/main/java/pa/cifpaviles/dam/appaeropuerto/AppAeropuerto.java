af1537def6b75f64f449be3e773d7af5c1f80424
Miguel fernandez Calleja <miguelfc18@educastur.es>
2023-12-13 19:37:25 +0100

    primera version

---
+++ src/main/java/pa/cifpaviles/dam/appaeropuerto/AppAeropuerto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pa.cifpaviles.dam.appaeropuerto;
+
+import com.opencsv.CSVWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mariorl
+ */
+public class AppAeropuerto {
+
+    public static void main(String[] args) throws IOException {
+             
+           
+        
+    }
+}
